95e77258512912e45a02763c5bfcfc84c2cc65bc
MAR.AVITABI <MAR.AVITABI@s124-10.pedago.mermoz.edu>
2019-11-20 17:57:05 +0100

    ENCASPULATION DE ATTRIBUT DE LA TOWER DEFENSE

--- src/towers/Ennemi.java
+++ src/towers/Ennemi.java
@@ -4,24 +4,67 @@ import java.util.Scanner;
 
 public class Ennemi {
 
-	int pv;
-	float posX;
-	float posY;
-	int rapidite;
+	private int pv;
+	private float posX;
+	private float posY;
+	private int rapidite;
 
 	static int compteur; // variable de classe
-	
-	
+
+	public int getPv() {
+		return this.pv;
+	}
+
+	public void setPv(int nouvelPv) {
+		if(nouvelPv > 1000 || nouvelPv < 0) {
+			 this.pv = 0;
+			 System.out.println("valeur trop haute retente");
+		}else {
+			this.pv= nouvelPv;
+			System.out.println("Combattant Pret");
+		}
+	}
+	public int getRapidite() {
+		return this.rapidite;	
+	}
+	public void setRapidite( int nouvelleRapidite) {
+		if(nouvelleRapidite < 20 || nouvelleRapidite > 0) {
+			 this.rapidite = 0;
+			 System.out.println("valeur trop haute retente");
+		}else {
+			this.rapidite = nouvelleRapidite;
+			System.out.println("Combattant Pret");
+		}
+	}
+	public float getPosX() {
+		return this.posX;
+	}
+	public void setPosX( float newCoordoX) {
+		if(newCoordoX < 250 || newCoordoX> -1) {
+			this.posX = 0;
+		}else {
+			this.posX = newCoordoX;
+		}
+	}
+	public float getPosY() {
+		return this.posX;
+	}
+	public void setPosY( float newCoordoY) {
+		if(newCoordoY < 250 || newCoordoY > -1) {
+			this.posX = 0;
+		}else {
+			this.posX = newCoordoY;
+		}
+	}
+
+
 	public Ennemi() {
 		compteur++;
-		this.pv = 100;
-		this.posX = 0;
-		this.posY = 0;
-		this.rapidite = 2;
+
 	}
 
 	public Ennemi(int pv, int rapidite) {
-		compteur++;
+	
 		this.pv = pv;
 		this.rapidite = rapidite;
 	}
@@ -30,35 +73,26 @@ public class Ennemi {
 		System.out.println(
 				" pv : " + this.pv + " posX : " + this.posX + " posY : " + this.posY + " rapidité: " + this.rapidite);
 	}
-	
+
 	public boolean estVivant() {
 		return (this.pv > 0);
 	}
-	
+
 	public String toString() {
-		return "Ennemi : " + " pv : " + this.pv + " posX : " + this.posX + " posY : " + this.posY + " rapidité: " + this.rapidite;
+		return "Ennemi : " + " pv : " + this.pv + " posX : " + this.posX + " posY : " + this.posY + " rapidité: "
+				+ this.rapidite;
 	}
-	
+
 	public static Ennemi saisirUnEnnemi() {
 		Ennemi unEnnemi = new Ennemi();
 		// Saisie au clavier par l'utilisateur ...
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Donner les points de vie: ");
 		int pointsDeVie = sc.nextInt();
-		unEnnemi.pv = pointsDeVie;
+		unEnnemi.pv = pointsDeVie ;
 		System.out.println("Donner la vitesse: ");
-		unEnnemi.rapidite = sc.nextInt();	
+		unEnnemi.rapidite = sc.nextInt();
 		return unEnnemi;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
--- src/towers/Terrain.java
+++ src/towers/Terrain.java
@@ -4,14 +4,39 @@ public class Terrain {
 
 	public static void main(String[] args) {
 		Ennemi unGrosMechant = new Ennemi();
+		
 		Ennemi unAutreEnnemi = new Ennemi();
+		
 		System.out.println("Nombre d'ennemi : " + Ennemi.compteur);
+		
 		Ennemi unVraiGrosMechant = new Ennemi(500,1);
 		Ennemi lePetitRapide = new Ennemi(200,10);
-		//System.out.println(unGrosMechant.pv);
-		unGrosMechant.pv = 200;
-		//System.out.println(unGrosMechant.pv);
-		unAutreEnnemi.rapidite=3;
+		System.out.println("les points de vie du GROS MECHANT SONT : "+ unGrosMechant.getPv());
+		
+		
+		
+		Ennemi aNewMechant = new Ennemi();
+		aNewMechant.setPv(2000);
+		aNewMechant.getPv();
+		System.out.println("les points de vie du NEW MECHANT SONT : "+ aNewMechant.getPv());
+		System.out.println("les points de rapidité du GrosMechant SONT : "+ aNewMechant.getRapidite());
+		
+		
+		
+		unGrosMechant.setPv(500);
+		unGrosMechant.getPv();
+		unGrosMechant.setRapidite(5);
+		unGrosMechant.getRapidite()	;
+		unGrosMechant.setPosX(15);
+		unGrosMechant.getPosX();
+		System.out.println("les points de VIE du GrosMechant SONT : "+ unGrosMechant.getPv());
+		System.out.println("les points de RAPIDITE du GrosMechant SONT : "+ unGrosMechant.getRapidite());
+		System.out.println("les points de POSITION du GrosMechant SONT : "+ unGrosMechant.getPosX());
+		System.out.println("les points de POSITION du GrosMechant SONT : "+ unGrosMechant.getPosY());
+
+		
+
+		//unAutreEnnemi.getRapidite();
 		//System.out.println(unAutreEnnemi.rapidite);
 		
 		// Affichage
@@ -21,10 +46,10 @@ public class Terrain {
 		// lePetitRapide.affiche();
 		
 
-		System.out.println(unGrosMechant);
+	//	System.out.println(unGrosMechant);
 		
-		System.out.println("Nombre d'ennemi : " +  unGrosMechant.compteur );
-		System.out.println("Nombre d'ennemi : " +  Ennemi.compteur );
+		//System.out.println("Nombre d'ennemi : " +  unGrosMechant.compteur );
+		//System.out.println("Nombre d'ennemi : " +  Ennemi.compteur );
 		
 		System.out.println(Ennemi.saisirUnEnnemi());
 	}
